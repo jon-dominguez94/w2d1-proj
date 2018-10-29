41af97c6b3543d3f55cd72622c5d921ce08e3b20
jon-dominguez94 <jon.dominguez94@yahoo.com>
2018-10-29 14:02:36 -0700

    Add errors to all methods

--- skeleton/super_useful.rb
+++ skeleton/super_useful.rb
@@ -1,6 +1,8 @@
 # PHASE 2
 def convert_to_int(str)
   Integer(str)
+rescue ArgumentError
+  return nil
 end
 
 # PHASE 3
@@ -10,8 +12,12 @@ def reaction(maybe_fruit)
   if FRUITS.include? maybe_fruit
     puts "OMG, thanks so much for the #{maybe_fruit}!"
   else 
-    raise StandardError 
-  end 
+    if maybe_fruit == "coffee"
+      raise NameError
+    else
+      raise StandardError
+    end
+  end
 end
 
 def feed_me_a_fruit
@@ -19,14 +25,17 @@ def feed_me_a_fruit
 
   puts "Feed me a fruit! (Enter the name of a fruit:)"
   maybe_fruit = gets.chomp
-  reaction(maybe_fruit) 
+  reaction(maybe_fruit)
+rescue NameError
+  retry
 end  
 
 # PHASE 4
 class BestFriend
   def initialize(name, yrs_known, fav_pastime)
     @name = name
-    @yrs_known = yrs_known
+    @yrs_known = yrs_known 
+    raise "Not enough years" if yrs_known < 5
     @fav_pastime = fav_pastime
   end
 
